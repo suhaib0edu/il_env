d6da5404c35cf46afb5ab8d655eed091f96c652d
Suhaib Eltayeb <suhab.edu@gmail.com>
2024-10-21 19:03:55 +0300

    تنظيم الترجمة و اضافة كلمات

--- lib/app/utils/lang.dart
+++ lib/app/utils/lang.dart
@@ -4,14 +4,36 @@ class TranslationsService extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'en_US': {
-          'help_text': 'How can I help you today?',
-          'question_hint': 'Do you have a question or inquiry?',
-          'study_lessons': 'Study Lessons',
+          'keyHelpText': 'How can I help you today?',
+          'keyQuestionHint': 'Do you have a question or inquiry?',
+          'keyStudyLessons': 'Study Lessons',
+          'keyUpdateSettings': 'Update Settings',
+          'keySettings': 'Settings',
+          'keySuccess': 'Success',
+          'keySettingsSavedSuccessfully': 'Settings saved successfully',
         },
         'ar_SA': {
-          'help_text': 'كيف يمكنني مساعدتك اليوم؟',
-          'question_hint': 'هل لديك سؤل او استفسار؟',
-          'study_lessons': 'مذاكرة الدورس',
+          'keyHelpText': 'كيف يمكنني مساعدتك اليوم؟',
+          'keyQuestionHint': 'هل لديك سؤل او استفسار؟',
+          'keyStudyLessons': 'مذاكرة الدروس',
+          'keyUpdateSettings': 'تحديث الإعدادات',
+          'keySettings': 'الإعدادات',
+          'keySuccess': 'نجاح',
+          'keySettingsSavedSuccessfully': 'تم حفظ الإعدادات بنجاح',
         },
       };
+}
+
+enum TranslationKey {
+  keyHelpText,
+  keyQuestionHint,
+  keyStudyLessons,
+  keyUpdateSettings,
+  keySettings,
+  keySuccess,
+  keySettingsSavedSuccessfully,
+}
+
+String translateKeyTr(TranslationKey key) {
+  return key.name.tr;
 }
