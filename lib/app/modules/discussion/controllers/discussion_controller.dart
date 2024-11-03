0f1cb7aaa5a3896f3b5131f8c677ac77d5825fda
Suhaib Eltayeb <suhab.edu@gmail.com>
2024-11-03 18:26:27 +0200

    اصدار اولي من صفحة المذاكره او المحادثة

--- lib/app/modules/discussion/controllers/discussion_controller.dart
+++ lib/app/modules/discussion/controllers/discussion_controller.dart
@@ -1,11 +1,186 @@
-import 'package:get/get.dart';
+import 'dart:convert';
+import 'package:il_env/index.dart';
 
 class DiscussionController extends GetxController {
-  //TODO: Implement DiscussionController
+  bool isMakingRequest = false;
+  bool isThinking = false;
+  bool showdeepExplanation = false;
+  bool showExploreQuestions = false;
+  bool showDirectQuestion = false;
+  bool isDialogOpen = false;
+  String lesson = '';
+  List<Map<String, dynamic>> contentParts = [];
+  int currentPart = 1;
+  int currentDeepExplanation = 0;
+  int currentExploreQuestion = 0;
+  int currentDirectQuestion = 0;
+  TextEditingController directQuestionController = TextEditingController();
 
-  final count = 0.obs;
+  // خريطة لتخزين الشروحات العميقة حسب الجزء
+  Map<int, List<dynamic>> deepExplanationMap = <int, List<dynamic>>{};
+  // خريطة لتخزين الأسئلة الاستكشافية حسب الجزء
+  Map<int, List<dynamic>> exploreQuestionsMap = <int, List<dynamic>>{};
+  // خريطة لتخزين الأسئلة المباشرة حسب الجزء
+  Map<int, List<dynamic>> directQuestionMap = <int, List<dynamic>>{};
 
+  // دالة التهيئة
+  @override
+  void onInit() async {
+    super.onInit();
+    try {
+      lesson = Get.arguments[0];
+      lessonDivider();
+    } catch (e) {
+      try {
+        lesson = await storage.read(key: 'lesson') ?? '';
+        lessonDivider();
+      } catch (e) {
+        lesson = '';
+        errorSnackbar(TranslationKey.keyCheckConnection);
+        Get.toNamed(Routes.HOME);
+      }
+    }
+  }
 
+  // دالة تقسيم الدرس
+  lessonDivider() async {
+    try {
+      isMakingRequest = true;
+      update(['discussionView']);
+      // Agent agent = Agent();
+      // String systemInstruction = AgentUtils().lessonDividerPrompt();
+      // var response = await agent.initiateChat(systemInstruction, lesson);
+      // var data = jsonDecode(response);
+      // var parts = data['content_parts'];
 
-  void increment() => count.value++;
+      String? jsonString = await storage.read(key: 'contentParts');
+      if (jsonString != null) {
+        List<dynamic> jsonData = jsonDecode(jsonString);
+        contentParts = List<Map<String, dynamic>>.from(jsonData);
+      }
+
+      // parts.forEach((element) {
+      //   contentParts.add(element);
+      // });
+      // await storage.write(key: 'contentParts', value: jsonEncode(contentParts));
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    isMakingRequest = false;
+    update(['discussionView']);
+  }
+
+  void deepExplanation() async {
+    try {
+      isThinking = true;
+      update(['buildRequestIndicator']);
+      Agent agent = Agent();
+      String systemInstruction = AgentUtils().deepExplanationPrompt();
+
+      agent
+          .initiateChat(systemInstruction, contentParts[currentPart - 1]['c'])
+          .then((value) {
+        if (!deepExplanationMap.containsKey(currentPart)) {
+          deepExplanationMap[currentPart] = [];
+        }
+        deepExplanationMap[currentPart]!.add(value);
+        // storage.write(
+        //     key: 'deepExplanationMap', value: jsonEncode(deepExplanationMap));
+      }).whenComplete(() {
+        isThinking = false;
+        update(['deepExplanationControls', 'buildRequestIndicator']);
+      });
+
+      // var x = await storage.read(key: 'deepExplanationMap');
+      //   print(x);
+      // if (x != null) {
+      //   deepExplanationMap = jsonDecode(x);
+      // }
+      // isThinking = false;
+      // update(['deepExplanationControls', 'buildRequestIndicator']);
+    } catch (e) {
+      debugPrint(e.toString());
+      isThinking = false;
+      update(['deepExplanationControls', 'buildRequestIndicator']);
+    }
+  }
+
+  void exploreQuestions() {
+    try {
+      isThinking = true;
+      update(['buildRequestIndicator']);
+      print(exploreQuestionsMap[currentPart]);
+      Agent agent = Agent();
+      String systemInstruction = AgentUtils()
+          .exploreQuestionsPrompt(part: contentParts[currentPart - 1],oldQuestions: exploreQuestionsMap[currentPart]);
+      agent
+          .initiateChat(systemInstruction, contentParts[currentPart - 1]['c'])
+          .then((value) {
+        if (!exploreQuestionsMap.containsKey(currentPart)) {
+          exploreQuestionsMap[currentPart] = [];
+        }
+        exploreQuestionsMap[currentPart]!.add(value);
+      }).whenComplete(() {
+        isThinking = false;
+        update(['exploreQuestionsControls', 'buildRequestIndicator']);
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+      isThinking = false;
+      update(['exploreQuestionsControls', 'buildRequestIndicator']);
+    }
+  }
+
+  void directQuestion() {
+    isDialogOpen = false;
+    Get.back(); 
+    try {
+      isThinking = true;
+      update(['buildRequestIndicator']);
+      Agent agent = Agent();
+      String systemInstruction = AgentUtils()
+          .directQuestionPrompt(part: contentParts[currentPart - 1]);
+      agent
+          .initiateChat(systemInstruction, directQuestionController.text)
+          .then((value) {
+        if (!directQuestionMap.containsKey(currentPart)) {
+          directQuestionMap[currentPart] = [];
+        }
+        directQuestionMap[currentPart]!.add(value);
+      }).whenComplete(() {
+        isThinking = false;
+        update(['directQuestionsControls', 'buildRequestIndicator']);
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+      isThinking = false;
+      update(['exploreQuestionsControls', 'buildRequestIndicator']);
+    }
+  }
+
+  next() {
+    if (currentPart < contentParts.length) {
+      currentPart++;
+      currentValueToZero();
+      update(['discussionView']);
+    }
+  }
+
+  previous() {
+    if (currentPart > 1) {
+      currentPart--;
+      currentValueToZero();
+      update(['discussionView']);
+    }
+  }
+
+  currentValueToZero() {
+    currentDeepExplanation = 0;
+    currentExploreQuestion = 0;
+    currentDirectQuestion = 0;
+    showdeepExplanation = false;
+    showExploreQuestions = false;
+    showDirectQuestion = false;
+    update(['deepExplanationControls']);
+  }
 }
--- lib/app/modules/discussion/views/discussion_view.dart
+++ lib/app/modules/discussion/views/discussion_view.dart
@@ -1,23 +1,376 @@
-import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
+import 'package:il_env/index.dart';
 import '../controllers/discussion_controller.dart';
 
 class DiscussionView extends GetView<DiscussionController> {
   const DiscussionView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DiscussionView'),
-        centerTitle: true,
+      body: GetBuilder<DiscussionController>(
+        id: 'discussionView',
+        builder: (controller) {
+          if (controller.isMakingRequest) {
+            return const LoadingIndicator();
+          } else if (controller.contentParts.isEmpty) {
+            return const NoDataIndicator();
+          } else {
+            return DiscussionContent(controller: controller);
+          }
+        },
       ),
-      body: const Center(
-        child: Text(
-          'DiscussionView is working',
-          style: TextStyle(fontSize: 20),
+    );
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(translateKeyTr(TranslationKey.keyContentPreparation)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NoDataIndicator extends StatelessWidget {
+  const NoDataIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(translateKeyTr(TranslationKey.keyNoData)));
+  }
+}
+
+class DiscussionContent extends StatelessWidget {
+  final DiscussionController controller;
+
+  const DiscussionContent({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView(
+            children: [
+              const SizedBox(height: 20),
+              _buildContentTitle(),
+              _buildContentDescription(),
+              GetBuilder<DiscussionController>(
+                id: 'deepExplanationControls',
+                builder: (ctr) => _buildDeepExplanation(),
+              ),
+              GetBuilder<DiscussionController>(
+                id: 'exploreQuestionsControls',
+                builder: (ctr) => _buildExploreQuestions(),
+              ),
+              GetBuilder<DiscussionController>(
+                id: 'directQuestionsControls',
+                builder: (ctr) => _buildDirectQuestions(),
+              ),
+              const SizedBox(height: 15),
+            ],
+          ),
         ),
+        GetBuilder<DiscussionController>(
+          id: 'buildRequestIndicator',
+          builder: (ctr) => _buildRequestIndicator(),
+        ),
+        SizedBox(height: 8),
+        _buildNavigationRow(),
+        _buildActionButtons(),
+      ],
+    );
+  }
+
+  Widget _buildContentTitle() {
+    return Center(
+      child: Text(
+        controller.contentParts[controller.currentPart - 1]['t'],
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: AppColors.primaryColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContentDescription() {
+    return CustomContainer(
+      margin: const EdgeInsets.all(16),
+      child: Text(
+        controller.contentParts[controller.currentPart - 1]['c'],
+        style: TextStyle(color: AppColors.primaryColor),
+      ),
+    );
+  }
+
+  Widget _buildDeepExplanation() {
+    if (!controller.deepExplanationMap.containsKey(controller.currentPart) ||
+        controller.deepExplanationMap[controller.currentPart]!.isEmpty) {
+      return const SizedBox();
+    }
+
+    return _buildContent(
+      titleKey: TranslationKey.keyDeepExplanation,
+      currentIndex: controller.currentDeepExplanation,
+      dataList: controller.deepExplanationMap[controller.currentPart]!,
+      onPrevious: () {
+        if (controller.currentDeepExplanation > 0) {
+          controller.currentDeepExplanation--;
+          controller.update(['deepExplanationControls']);
+        }
+      },
+      onNext: () {
+        if (controller.currentDeepExplanation <
+            controller.deepExplanationMap[controller.currentPart]!.length - 1) {
+          controller.currentDeepExplanation++;
+          controller.update(['deepExplanationControls']);
+        }
+      },
+      showContent: controller.showdeepExplanation,
+      toggleShowContent: () {
+        controller.showdeepExplanation = !controller.showdeepExplanation;
+        controller.update(['deepExplanationControls']);
+      },
+    );
+  }
+
+  Widget _buildExploreQuestions() {
+    if (!controller.exploreQuestionsMap.containsKey(controller.currentPart) ||
+        controller.exploreQuestionsMap[controller.currentPart]!.isEmpty) {
+      return const SizedBox();
+    }
+
+    return _buildContent(
+      titleKey: TranslationKey.keyExploreQuestions,
+      currentIndex: controller.currentExploreQuestion,
+      dataList: controller.exploreQuestionsMap[controller.currentPart]!,
+      onPrevious: () {
+        if (controller.currentExploreQuestion > 0) {
+          controller.currentExploreQuestion--;
+          controller.update(['exploreQuestionsControls']);
+        }
+      },
+      onNext: () {
+        if (controller.currentExploreQuestion <
+            controller.exploreQuestionsMap[controller.currentPart]!.length -
+                1) {
+          controller.currentExploreQuestion++;
+          controller.update(['exploreQuestionsControls']);
+        }
+      },
+      showContent: controller.showExploreQuestions,
+      toggleShowContent: () {
+        controller.showExploreQuestions = !controller.showExploreQuestions;
+        controller.update(['exploreQuestionsControls']);
+      },
+    );
+  }
+
+  Widget _buildDirectQuestions() {
+    if (!controller.directQuestionMap.containsKey(controller.currentPart) ||
+        controller.directQuestionMap[controller.currentPart]!.isEmpty) {
+      return const SizedBox();
+    }
+
+    return _buildContent(
+      titleKey: TranslationKey.keyDirectQuestion,
+      currentIndex: controller.currentDirectQuestion,
+      dataList: controller.directQuestionMap[controller.currentPart]!,
+      onPrevious: () {
+        if (controller.currentDirectQuestion > 0) {
+          controller.currentDirectQuestion--;
+          controller.update(['directQuestionsControls']);
+        }
+      },
+      onNext: () {
+        if (controller.currentDirectQuestion <
+            controller.directQuestionMap[controller.currentPart]!.length - 1) {
+          controller.currentDirectQuestion++;
+          controller.update(['directQuestionsControls']);
+        }
+      },
+      showContent: controller.showDirectQuestion,
+      toggleShowContent: () {
+        controller.showDirectQuestion = !controller.showDirectQuestion;
+        controller.update(['directQuestionsControls']);
+      },
+    );
+  }
+
+  Widget _buildRequestIndicator() {
+    return controller.isThinking
+        ? const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('يفكر ...'),
+          )
+        : const SizedBox();
+  }
+
+  Widget _buildNavigationRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomTextButton(
+          textPadding: EdgeInsets.zero,
+          onPressed: () => controller.previous(),
+          fontSize: 19,
+          text: '<',
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            '${controller.currentPart} / ${controller.contentParts.length}',
+            style: TextStyle(fontSize: 16, color: AppColors.primaryColor),
+          ),
+        ),
+        CustomTextButton(
+          textPadding: EdgeInsets.zero,
+          onPressed: () => controller.next(),
+          fontSize: 19,
+          text: '>',
+        ),
+      ],
+    );
+  }
+
+  Widget _buildContent({
+    required TranslationKey titleKey,
+    required int currentIndex,
+    required List<dynamic> dataList,
+    required void Function() onPrevious,
+    required void Function() onNext,
+    required bool showContent,
+    required void Function() toggleShowContent,
+  }) {
+    // تحقق من صحة الفهرس
+    if (dataList.isEmpty ||
+        currentIndex < 0 ||
+        currentIndex >= dataList.length) {
+      return Container(); // أو يمكن عرض رسالة مناسبة
+    }
+
+    return CustomContainer(
+      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Column(
+        children: [
+          CustomContainer(
+            margin: EdgeInsets.symmetric(vertical: 4),
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            color: AppColors.primaryColor,
+            child: Row(
+              children: [
+                Text(
+                  translateKeyTr(titleKey),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.quaternaryColor,
+                  ),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed: onPrevious,
+                  child: const Text('<'),
+                ),
+                Text(
+                  '${currentIndex + 1}/${dataList.length}',
+                  style: TextStyle(color: Colors.white),
+                ),
+                TextButton(
+                  onPressed: onNext,
+                  child: const Text('>'),
+                ),
+                SizedBox(width: 8),
+                IconButton(
+                  onPressed: toggleShowContent,
+                  icon: Icon(
+                    showContent
+                        ? Icons.arrow_drop_down_rounded
+                        : Icons.arrow_right_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Visibility(
+            visible: showContent,
+            child: CustomMarkdown(data: dataList[currentIndex]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButtons() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomTextButton(
+            textPadding: EdgeInsets.zero,
+            text: translateKeyTr(TranslationKey.keyDeepExplanation),
+            onPressed: () => controller.deepExplanation(),
+          ),
+          CustomTextButton(
+            textPadding: EdgeInsets.zero,
+            text: translateKeyTr(TranslationKey.keyExploreQuestions),
+            onPressed: () => controller.exploreQuestions(),
+          ),
+          CustomTextButton(
+            textPadding: EdgeInsets.zero,
+            text: translateKeyTr(TranslationKey.keyDirectQuestion),
+            onPressed: () {
+              if (!controller.isDialogOpen) {
+                controller.isDialogOpen = true;
+                Get.dialog(
+                  Dialog(
+                    backgroundColor: AppColors.secondaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 40,
+                        horizontal: 20,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomTextField(
+                            controller: controller.directQuestionController,
+                            maxLength: 500,
+                            labelText: translateKeyTr(
+                                TranslationKey.keyDirectQuestion),
+                            suffixIcon: IconButton(
+                              onPressed: () => controller.directQuestion(),
+                              icon: Icon(
+                                Icons.send,
+                                color: AppColors.primaryColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
