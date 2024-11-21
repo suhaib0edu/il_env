fce58b157b2d47655445b2608cb87ffe88f61181
Suhaib Eltayeb <suhab.edu@gmail.com>
2024-11-21 19:52:43 +0200

    اصلاح مشكلة تحديث الروابط

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -7,9 +7,6 @@ class SettingsView extends GetView<SettingsController> {
 
   @override
   Widget build(BuildContext context) {
-    String apikeyLinkToCopy = controller.selectedModel.value == ModelType.gemini
-        ? 'https://aistudio.google.com/app/apikey'
-        : 'https://platform.openai.com/api-keys';
     return Scaffold(
       appBar: AppBar(
         title: Text(translateKeyTr(TranslationKey.keySettings)),
@@ -32,25 +29,32 @@ class SettingsView extends GetView<SettingsController> {
             CustomTextField(
                 labelText: 'API Key', controller: controller.apiKeyController),
             GetBuilder<SettingsController>(
-              builder: (controller) => Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(translateKeyTr(TranslationKey.keyApiKeyInfo)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(apikeyLinkToCopy),
-                      IconButton(
-                          onPressed: () {
-                            Clipboard.setData(
-                                ClipboardData(text: apikeyLinkToCopy));
-                            successSnackbar(TranslationKey.keySuccessCopied);
-                          },
-                          icon: Icon(Icons.copy))
-                    ],
-                  ),
-                ],
-              ),
+              builder: (controller) {
+                String apikeyLinkToCopy =
+                    controller.selectedModel.value == ModelType.gemini
+                        ? 'https://aistudio.google.com/app/apikey'
+                        : 'https://platform.openai.com/api-keys';
+                print(apikeyLinkToCopy);
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(translateKeyTr(TranslationKey.keyApiKeyInfo)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(apikeyLinkToCopy),
+                        IconButton(
+                            onPressed: () {
+                              Clipboard.setData(
+                                  ClipboardData(text: apikeyLinkToCopy));
+                              successSnackbar(TranslationKey.keySuccessCopied);
+                            },
+                            icon: Icon(Icons.copy))
+                      ],
+                    ),
+                  ],
+                );
+              },
             ),
             Spacer(),
             Padding(
